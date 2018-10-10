796a0a595a0d7c209b344e0d24668c01846d4fc0
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-10-10 13:21:10 -0700

    starting the app using react boilerPlate

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize-scss/sass/_normalize.scss';
+import './styles/styles.scss';
+
+const appContainer = document.querySelector('#app');
