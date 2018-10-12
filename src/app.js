784beb42db78b49563a9b0b728b883ff00e0736a
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-10-12 10:00:51 -0700

    setting up the router configurations

--- src/app.js
+++ src/app.js
@@ -1,6 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize-scss/sass/_normalize.scss';
 import './styles/styles.scss';
 
+const TodoDashBoardPage = () => (
+    <div>
+        Todo Dashboard.
+    </div>
+);
+
+const AddTodoPage = () => (
+    <div>
+        AddTodoPage
+    </div>
+);
+
+const routes = (
+    <div>
+        <BrowserRouter>
+            <div>
+                <Route path="/" component={TodoDashBoardPage} exact={true} />
+                <Route path="/add" component={AddTodoPage} />
+            </div>
+        </BrowserRouter>
+    </div>
+);
+
 const appContainer = document.querySelector('#app');
+ReactDOM.render(routes, appContainer);
