e8ac1ea432c73720ef1baa55ea7b2da8d4eae69d
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-10-12 10:54:42 -0700

    referenced app router

--- src/app.js
+++ src/app.js
@@ -1,31 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import AppRouter from './routes/AppRouter'
 import 'normalize-scss/sass/_normalize.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/styles.scss';
 
-const TodoDashBoardPage = () => (
-    <div>
-        Todo Dashboard.
-    </div>
-);
 
-const AddTodoPage = () => (
-    <div>
-        AddTodoPage
-    </div>
-);
-
-const routes = (
-    <div>
-        <BrowserRouter>
-            <div>
-                <Route path="/" component={TodoDashBoardPage} exact={true} />
-                <Route path="/add" component={AddTodoPage} />
-            </div>
-        </BrowserRouter>
-    </div>
-);
 
 const appContainer = document.querySelector('#app');
-ReactDOM.render(routes, appContainer);
+ReactDOM.render(<AppRouter />, appContainer);
