98223617a975c334a406887f20c6963c1f96d603
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-10-12 10:53:11 -0700

    blueprint for the headerComponent

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HeaderComponent = () => (
+    <div>
+        <div className='container'>
+            <div className='row'>
+                <div className='col text-center'>Todo</div>
+            </div>
+        </div>
+    </div>
+);
+
+export default HeaderComponent;
