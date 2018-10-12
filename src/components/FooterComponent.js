068dd24ebc91efec4ce42e3d13e3dd9f067c1bd9
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-10-12 10:53:28 -0700

    blueprints for the footerComponent

---
+++ src/components/FooterComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FooterComponent = () => (
+    <div>
+        <div className='container fixed-bottom'>
+            <div className='row'>
+                <div className='col text-center'>Footer</div>
+            </div>
+        </div>
+    </div>
+);
+
+export default FooterComponent;
