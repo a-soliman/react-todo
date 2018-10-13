c2b7a38430b732d0ef45175decd4ff92603c5b3e
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-10-13 13:26:18 -0700

    tested the app after distrubuting it's functionalities

--- src/app.js
+++ src/app.js
@@ -1,9 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './routes/AppRouter'
+import configureStore from './store/configureStore';
 import 'normalize-scss/sass/_normalize.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/styles.scss';
+import { createTodo } from './actions/todos';
+import { setTextFilter, setStartDateFilter, setEndDeateFilter } from './actions/filters';
+import getVisibleTodos from './selectors/todos';
+
+const store = configureStore();
+
+store.subscribe(() => {
+    const state = store.getState();
+    const visibleTodos = getVisibleTodos(state.todos, state.filters);
+    console.log(visibleTodos);
+});
+
+store.dispatch(createTodo({
+    title: 'test todo',
+    note: 'This is a test todo',
+    createdAt: 10
+}));
+store.dispatch(createTodo({
+    title: 'test todo',
+    note: 'This is a test todo',
+    createdAt: 8
+}));
+store.dispatch(createTodo({
+    title: 'test todo',
+    note: 'This is a test todo',
+    createdAt: 12
+}));
+
+
 
 
 
