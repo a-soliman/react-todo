658439199c88bd0e2e62f24ba67ab9e7468675ac
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-10-14 06:54:50 -0700

    coded todo form

---
+++ src/components/TodoForm.js
@@ -0,0 +1,70 @@
+import React from 'react';
+
+export default class TodoForm extends React.Component {
+    state = {
+        title: '',
+        note: '',
+        error: ''
+    };
+
+    onTitleChange = (e) => {
+        const title  = e.target.value;
+        const error = '';
+        this.setState(() => ({ title, error }));
+    };
+
+    onNoteChange = (e) => {
+        const note = e.target.value.trim();
+        this.setState(() => ({ note }));
+    }
+
+    onSubmit = (e) => {
+        e.preventDefault();
+        let error = ''
+        if ( !this.state.title || this.state.title.trim().length < 1 ) {
+            error = 'Please provide a title for the Todo.'
+            this.setState(() => ({ error }));
+        } else {
+            error = ''
+            this.setState(() => ({ error }));
+            this.props.onSubmit({
+                title: this.state.title,
+                note: this.state.note
+            });
+        }
+    };
+
+    render() {
+        return (
+            <div>
+                <form className="form" onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <input 
+                            className="form-control" 
+                            value={this.state.title} 
+                            onChange={this.onTitleChange}
+                            type="text" 
+                            placeholder="Title" 
+                            autoFocus
+                        />
+                        {
+                            this.state.error && 
+                            <div className="alert alert-danger">{this.state.error}</div>
+                        }
+                    </div>
+        
+                    <div className="form-group">
+                        <textarea 
+                            className="form-control" 
+                            value={this.state.notet} 
+                            onChange={this.onNoteChange}
+                            placeholder="note">
+                        </textarea>
+                    </div>
+        
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        );
+    }
+}
