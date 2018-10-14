31b20c69656879c32b79ca499bfc51bd55275564
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-10-13 17:51:44 -0700

    added the list components viewing all the todos and the filter text input into the dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import TodosList from '../components/TodosList';
+import TodoListFilters from '../components/TodoListFilters';
 
 const DashboardPage = () => (
     <div>
         Todo Dashboard.
+        <TodoListFilters />
+        <TodosList />
     </div>
 );
 
