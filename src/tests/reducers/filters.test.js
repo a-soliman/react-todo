d54ab2ca90e351189fd0515866ed5d470313a9ec
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-10-26 10:12:13 -0700

    testing filtersReducer

--- src/tests/reducers/filters.test.js
+++ src/tests/reducers/filters.test.js
@@ -1,16 +1,14 @@
 import moment from 'moment';
 import filtersReducer from '../../reducers/filters';
+import { stat } from 'fs';
 
 describe('FiltersReducer', () => {
-    test('Should setup default filter values', () => {
-        const state = filtersReducer(undefined, {
-            type: '@@INIT'
-        });
-        expect(state).toEqual({
-            text: '',
-            sort: 'mostRecent',
-            startDate: moment().startOf('month'),
-            endDate: moment().endOf('month')
-        });
-    });
+    const state = {
+        text: '',
+        startDate: moment().startOf('month'),
+        endDate: moment().endOf('month'),
+        sort: 'mostRecent'
+    };
+
+    
 });
