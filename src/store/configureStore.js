832798261b2d536d306fdffb3a29221c2056e5c5
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-10-26 09:02:43 -0700

    added the required configurations for redux store

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -9,7 +9,8 @@ export default () => {
                 todos: todosReducer, 
                 filters: filtersReducer 
             }
-        )
+        ),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     );
 
     return store;
