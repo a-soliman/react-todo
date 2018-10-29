aaefaebc3132cef3350aa61ae45559fc5c18a3fa
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-10-29 15:07:31 -0700

    testing EditTodoComponent

---
+++ src/tests/components/TodoListFilters.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import moment from 'moment';
+import { TodoListFilters } from '../../components/TodoListFilters';
+import { filters, altFilters } from '../fixtures/filters';
+
+
+describe('TodoListFiltersComponent', () => {
+    let setTextFilter, sortByMostRecent, sortByOlder, setStartDateFilter, setEndDeateFilter, wrapper;
+
+    beforeEach(() => {
+        setTextFilter = jest.fn();
+        sortByMostRecent = jest.fn();
+        sortByOlder = jest.fn();
+        setStartDateFilter = jest.fn();
+        setEndDeateFilter = jest.fn();
+        wrapper = shallow(
+            <TodoListFilters 
+                filters={filters}
+                setTextFilter={setTextFilter}
+                sortByMostRecent={sortByMostRecent}
+                sortByOlder={sortByOlder}
+                setStartDateFilter={setStartDateFilter}
+                setEndDeateFilter={setEndDeateFilter}
+            />
+        );
+    });
+
+    test('Should render TodoListFiltersComponent correctly', () => {
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    
+});
